e75b4fe66fb471f2b19f4ede792e031b54e2020d
Sonia Uduma <sohnyauduma@gmail.com>
2021-05-25 06:17:30 +0100

    Reactjs Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/services">Services</Link>
+        </nav>
+
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" exact component={About} />
+          <Route path="/services" exact component={Services} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ src/pages/Services.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Services = () => {
+  return <div>Know more about our services</div>;
+};
+
+export default Services;
